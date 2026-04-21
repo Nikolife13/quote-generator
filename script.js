35245a9901cf544a7a674ee2ff668fd3b4edc4d1
Nikolife13 <nikolife13@gmail.com>
2026-04-21 23:50:52 +0100

    feat: add random quote selection and link to DOM elements

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+const quoteText = document.getElementById('quot');
+const authorText = document.getElementById('author');
+
 let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
+}
 
 // Get Quotes From API
 async function getQuotes() {
@@ -6,9 +17,9 @@ async function getQuotes() {
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes); // Verify data in console
+        newQuote();
     } catch (error) {
-        // Catch Error Here
+        console.error('Fetch error:', error);
     }
 }
 
